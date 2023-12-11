f29c8d8fbcae3175a5aa1076abccc66b252f133a
Rita Martins <mazedoodle@gmail.com>
2023-12-11 20:45:23 +0000

    updated db

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-require("dotenv").config();
-const jsonServer = require("json-server");
-const morgan = require("morgan");
+require('dotenv').config();
+const jsonServer = require('json-server');
+const morgan = require('morgan');
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT;
 
 server.use(middlewares);
-server.use(morgan("dev"));
+server.use(morgan('dev'));
 server.use((req, res, next) => {
   // Middleware to disable CORS
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 server.use(router);
 
+// we need to run our server on a specific port, like live server does
 server.listen(PORT, () => {
   console.log(`JSON Server is running at port ${PORT}`);
 });
